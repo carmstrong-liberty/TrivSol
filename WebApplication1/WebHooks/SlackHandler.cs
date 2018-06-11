9639504d09dddc82a2863b5df0437e3310f5f12e
Colby Armstrong <carmstrong@libertysecurity.ca>
2018-06-11 13:35:42 -0600

    First Build

---
+++ WebApplication1/WebHooks/SlackHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Net.Http.Formatting;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+
+namespace WebApplication1.WebHooks
+{
+    public class SlackWebHookHandler : WebHookHandler
+    {
+        public override Task ExecuteAsync(string generator, WebHookHandlerContext context)
+        {
+            NameValueCollection nvc;
+            if (context.TryGetData<NameValueCollection>(out nvc))
+            {
+                string question = nvc["subtext"];
+                string msg = string.Format("TEST");
+                SlackResponse reply = new SlackResponse(msg);
+                context.Response = context.Request.CreateResponse(reply);
+            }
+            return Task.FromResult(true);
+        }
+    }
+}
